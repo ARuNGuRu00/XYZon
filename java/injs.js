ef2941f2e57abfd42e6220f869f2b41b6a3f42af
Arunguru <arunguru00@gmail.com>
2025-03-06 11:40:30 +0530

    nav

---
+++ java/injs.js
@@ -0,0 +1,7 @@
+var home=0;
+var program=0;
+var about=0;
+function activat(ele) {
+    document.getElementById(ele).style.borderStyle="solid";
+    document.getElementById(ele).style.borderWidth='0 0 3px 0';
+}
