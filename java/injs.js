d302e6f2b08666fea251eb9c1edef455b43672c4
Arunguru <arunguru00@gmail.com>
2025-03-13 00:16:36 +0530

    detail

--- java/injs.js
+++ java/injs.js
@@ -1,7 +1,13 @@
 var home=0;
 var program=0;
 var about=0;
-function activat(ele) {
-    document.getElementById(ele).style.borderStyle="solid";
-    document.getElementById(ele).style.borderWidth='0 0 3px 0';
-}
+
+function activate(ele) {
+    var x = document.getElementById(ele);
+    console.log(x.style.display);
+    if (x.style.display === 'none') {
+      x.style.display = 'flex';
+    } else {
+      x.style.display = 'none';
+    }
+  }
