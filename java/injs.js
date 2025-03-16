be64c405a35f93bf085e40e035c644a7b3096438
Arunguru <arunguru00@gmail.com>
2025-03-16 19:01:39 +0530

    fix

--- java/injs.js
+++ java/injs.js
@@ -24,13 +24,17 @@ function hchat(){
 function aniBot(){
   document.getElementById('chatarea').style.display='none';
   document.getElementById('bot').style.display='block';
-  document.getElementById('chat-win').innerHTML='';
 }
+
+let setvalue=0;
 
 function chat_bot(){
   document.getElementById('bot').style.display='none';
   document.getElementById('chatarea').style.display='flex';
-  setTimeout(bot_chats, 400); 
+  if (setvalue === 0){
+    setTimeout(bot_chats, 400); 
+    setvalue = 1;
+  }
 }
 
 /* const newDiv = document.createElement("div");
@@ -44,7 +48,7 @@ function bot_chats(){
 }
 
 function bottom_focus(){
-  var contentDiv = document.getElementById("chat-win");
+  var contentDiv = document.getElementById("chat-main");
   contentDiv.scrollTop = contentDiv.scrollHeight;
 }
 
@@ -61,7 +65,7 @@ function user_chat(){
 
 function auto_chat(chat='Hello there, what can i do for you??'){
   let hc = document.createElement('div');
-  hc.className="bot-chats col-11";
+  hc.className="bot-chats";
   hc.innerHTML=`<div class=' px-3 py-2 chat mx-2  rounded-end-pill'>${chat}</div>`;
   document.getElementById('chat-win').appendChild(hc);
   bottom_focus();
