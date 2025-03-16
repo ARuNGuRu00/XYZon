570f7b9982a9e1fe1b43e738dcae08a3c3968481
Arunguru <arunguru00@gmail.com>
2025-03-16 13:48:40 +0530

    bot fix

--- java/injs.js
+++ java/injs.js
@@ -1,7 +1,6 @@
 var home=0;
 var program=0;
 var about=0;
-
 function activate(ele) {
     var x = document.getElementById(ele);
     console.log(x.style.display);
@@ -11,3 +10,24 @@ function activate(ele) {
       x.style.display = 'none';
     }
   }
+
+
+function hchat(){
+  alert('asd');
+  let mdiv=document.createElement("div");
+  let val=document.getElementById("search").value;
+  mdiv.className="human-chats";
+  mdiv.innerHTML=`<div class='chat'>${val}</div>`;
+  document.querySelector('.chat-win').appendChild(mdiv);
+}
+
+function ani_bot(){
+  document.getElementById('chatarea').style.display='none';
+  document.getElementById('bot').style.display='block';
+}
+
+function chat_bot(){
+  document.getElementById('bot').style.display='none';
+  document.getElementById('chatarea').style.display='flex';
+  
+}
